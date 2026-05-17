b87fb4372ca791d41d75e107a37770d8562b9249
Phạm Văn Sử <supv2@Phams-MacBook-Pro.local>
2026-05-17 20:04:34 +0700

    Fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 import ProductItem from "./component/ProductItem";
 import { getProductByType } from './api/productState';
 
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 import { getProductByType } from "../api/productState";
 import ProductList from "../component/ProductList";
 
